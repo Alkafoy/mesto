29f89900dddce5dec495ed4cd8bc10187c2f70e4
Comp1 <esergey05@gmail.com>
2023-02-15 20:10:14 +0300

    fix1

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,38 @@
-const editButton = document.querySelector('.profile__edit-button');
+const buttonEdit = document.querySelector('.profile__edit-button');
 const popup = document.querySelector(".popup");
-const closeButton = popup.querySelector(".popup__close-icon");
-
-function toggleOpenPopup () {
-    popup.classList.toggle('popup_opened');
-}
-
-function handleOverlyClick (event) {
-    if (event.target === event.currentTarget) {
-        toggleOpenPopup();
-    }
-};
-
-editButton.addEventListener('click', toggleOpenPopup);
-closeButton.addEventListener('click', toggleOpenPopup);
-popup.addEventListener('click', handleOverlyClick);
-
-
+const buttonClose = popup.querySelector(".popup__close-icon");
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__form');
-
-// Находим поля формы в DOM
 let nameInput = formElement.querySelector('.popup__input_type_name');
-console.log(typeof nameInput.value);
 let jobInput = formElement.querySelector('.popup__input_type_job');
-console.log(typeof jobInput.value);
-
 let profileTitle = document.querySelector('.profile__title');
 let profileDescription = document.querySelector('.profile__description');
+
+function openPopup () {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileDescription.textContent;
+};
+
+function closePopup () {
+    popup.classList.remove('popup_opened');
+};
 
 function handleFormSubmit (event) {
     event.preventDefault(); 
     profileTitle.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
-    toggleOpenPopup();
+    closePopup();
 };
 
+buttonEdit.addEventListener('click', openPopup);
+buttonClose.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
 
+
+// function handleOverlyClick (event) {
+//     if (event.target === event.currentTarget) {
+//         closePopup();
+//     }
+// };
+// popup.addEventListener('click', handleOverlyClick);
